14e316e29c9fe3c59d6118b0dc6063d9f3ea022f
Mateus Maso <m.maso25@gmail.com>
2013-07-29 01:42:57 -0300

    whitespace fix

--- jquery.lifecycle.js
+++ jquery.lifecycle.js
@@ -1,39 +1,39 @@
 (function($) {
 
-	var observer = new MutationSummary({
-		queries: [{element: '[lifecycle]'}],
-		callback: function (summaries) {
-			$(summaries[0].added).each(function(index, element) {
-				if (element.whenInsert) {
-					$(element.whenInsert).each(function(index, callback) {
-						callback();
-					});
-					delete element.whenInsert;
-				}
-			});
+  var observer = new MutationSummary({
+    queries: [{element: '[lifecycle]'}],
+    callback: function (summaries) {
+      $(summaries[0].added).each(function(index, element) {
+        if (element.whenInsert) {
+          $(element.whenInsert).each(function(index, callback) {
+            callback();
+          });
+          delete element.whenInsert;
+        }
+      });
 
-			$(summaries[0].removed).each(function(index, element) {
-				if (element.whenRemove) {
-					$(element.whenRemove).each(function(index, callback) {
-						callback();
-					});
-					delete element.whenRemove;
-				}
-			});
-		}
-	});
+      $(summaries[0].removed).each(function(index, element) {
+        if (element.whenRemove) {
+          $(element.whenRemove).each(function(index, callback) {
+            callback();
+          });
+          delete element.whenRemove;
+        }
+      });
+    }
+  });
 
-	$.fn.lifecycle = function(options) {
-		options = options || {};
+  $.fn.lifecycle = function(options) {
+    options = options || {};
 
-		element = this.get(0);
-		element.whenInsert = element.whenInsert || [];
-		element.whenRemove = element.whenRemove || [];
+    element = this.get(0);
+    element.whenInsert = element.whenInsert || [];
+    element.whenRemove = element.whenRemove || [];
 
-		if (options.insert) element.whenInsert.push(options.insert);
-		if (options.remove) element.whenRemove.push(options.remove);
+    if (options.insert) element.whenInsert.push(options.insert);
+    if (options.remove) element.whenRemove.push(options.remove);
 
-		this.attr('lifecycle', '');
-	};
+    this.attr('lifecycle', '');
+  };
 
 })(jQuery)
